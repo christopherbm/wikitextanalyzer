7d4df1888f86ab0dc2f0737d786207fa0dafad4d
Chris Myers <christopherbm@gmail.com>
2013-08-05 16:37:08 -0700

    added basic stop words and filtered those out of Word creation

--- contentscript.js
+++ contentscript.js
@@ -9,8 +9,23 @@ window.textanalyzer = (function () {
 	
 	function TextAnalyzer() {
 		this.list = []; // Word list
+		
 		this.totalElementsSelected;
 		this.totalWordsProcessed = 0;
+		this.stopWordsFound = 0;
+		
+		this.stopWords = 
+			['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for',
+			'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by',
+			'from', 'they', 'we', 'say', 'her', 'she', 'or', 'an', 'will', 'my', 'one',
+			'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about',
+			'who', 'get', 'which', 'go', 'me', 'when', 'make', 'can', 'like', 'time', 'no',
+			'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some',
+			'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come',
+			'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our',
+			'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any',
+			'these', 'give', 'day', 'most', 'us'];
+		this.swLength = this.stopWords.length;
 	}
 	TextAnalyzer.prototype.processPage = function() {
 		var p_list = document.getElementsByTagName('p');
@@ -35,17 +50,28 @@ window.textanalyzer = (function () {
 	TextAnalyzer.prototype.processWord = function(w) {
  		this.totalWordsProcessed = this.totalWordsProcessed + 1;
  		
- 		var length = this.list.length;
- 		for (var i = 0; i < length; i++) {
- 			var word = this.list[i];
- 			if ( word.checkMatch(w) ) {
- 				return;
+ 		if( this.isStopWord(w) == false) {
+ 			var length = this.list.length;
+ 				for (var i = 0; i < length; i++) {
+ 				var word = this.list[i];
+ 				if ( word.checkMatch(w) ) {
+ 					return;
+ 				}
  			}
- 		}
  		
- 		var word = new Word(w);
- 		this.list.push(word);
- 		return
+ 			var word = new Word(w);
+ 			this.list.push(word);	
+ 		}
+ 		return;
+	}
+	TextAnalyzer.prototype.isStopWord = function(w) {
+		for (var i = 0; i < this.swLength; i++) {
+			if (w === this.stopWords[i]) {
+				this.stopWordsFound = this.stopWordsFound + 1;
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	function Word(w) {
@@ -62,6 +88,5 @@ window.textanalyzer = (function () {
 	
 	main();
 }());
-
 
 //chrome.extension.sendRequest({}, function(response) {});
